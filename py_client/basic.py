b3d3040eb01b41252dd3b7ffc2ca0f0b04d72340
unknown <uddeshteke007@gmail.com>
2022-11-26 14:30:32 +0530

    Added changes for endpoints and gitignore

---
+++ py_client/basic.py
@@ -0,0 +1,9 @@
+import requests
+
+endpoint = "https://sandbox.plaid.com/"
+endpoint = "http://localhost:8000/"
+
+response = requests.get(endpoint)
+#print(response.text) # printin response text
+print(response.status_code)
+# calling api
